66aecd575d0de8e90b13acb7c812d99ad87c5e7e
David Singleton <davidjsingleton18@gmail.com>
2024-11-03 15:41:12 -0800

    Display penguin images using temporary image elements for image carousel

--- src/index.js
+++ src/index.js
@@ -1,2 +1,11 @@
 import './style.css';
 import './reset.css';
+import boltPenguin from './assets/images/displayed-penguins/bolt-penguin.webp';
+import fluffalPenguin from './assets/images/displayed-penguins/fluffal-penguin.webp';
+
+const images = document.querySelectorAll('img');
+
+console.log(images);
+
+images[0].src = boltPenguin;
+images[1].src = fluffalPenguin;
