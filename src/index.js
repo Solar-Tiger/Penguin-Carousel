3eb38f9dd47c88de6aafb101beddb8e469704bf4
David Singleton <davidjsingleton18@gmail.com>
2024-11-04 10:59:17 -0800

    Add import function for penguin carousel
    Create array of images to control which image is on display when arrows are clicked

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import './style.css';
 import './reset.css';
+import createPenguinCarouselContainer from './components/PenguinCarouselContainer';
 import boltPenguin from './assets/images/displayed-penguins/bolt-penguin.webp';
 import fluffalPenguin from './assets/images/displayed-penguins/fluffal-penguin.webp';
 
-const images = document.querySelectorAll('img');
+const penguinImages = [boltPenguin, fluffalPenguin];
 
-console.log(images);
-
-images[0].src = boltPenguin;
-images[1].src = fluffalPenguin;
+createPenguinCarouselContainer(penguinImages);
