e1bf134665dfeb74ba1d6904e86f31c049d2db85
David Singleton <davidjsingleton18@gmail.com>
2024-11-05 10:58:38 -0800

    Update function name to be more clear

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import './style.css';
 import './reset.css';
-import createPenguinCarouselContainer from './components/PenguinCarouselContainer';
+import appendPenguinCarousel from './components/PenguinCarouselContainer';
 import boltPenguin from './assets/images/displayed-penguins/bolt-penguin.webp';
 import fluffalPenguin from './assets/images/displayed-penguins/fluffal-penguin.webp';
 
 const penguinImages = [boltPenguin, fluffalPenguin];
 
-createPenguinCarouselContainer(penguinImages);
+appendPenguinCarousel(penguinImages);
