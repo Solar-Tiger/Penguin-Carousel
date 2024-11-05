ed0bc81327215d094feb34ac9fdfa9202f8702ad
David Singleton <davidjsingleton18@gmail.com>
2024-11-05 10:59:50 -0800

    Add and display next and previous buttons for penguin carousel

--- src/components/PenguinCarouselContainer.js
+++ src/components/PenguinCarouselContainer.js
@@ -1,11 +1,35 @@
-export default function createPenguinCarouselContainer(penguinsArray) {
+import nextArrow from '../assets/images/icons/arrow_forward.svg';
+import previousArrow from '../assets/images/icons/arrow_back.svg';
+
+export default function appendPenguinCarousel(penguinsArray) {
+    let currentPenguinImage = 0;
+
     // Create Penguin Carousel container
     document.body.appendChild(createDivContainer('penguin-carousel-container'));
 
-    // Create and append penguin image
-    createDivContainer().appendChild(createImage(penguinsArray[0]));
+    const penguinCarouselContainer = document.getElementById(
+        'penguin-carousel-container'
+    );
 
-    console.log(penguinsArray[0]);
+    // Create and append penguin image
+    function createAndAppendPenguin() {
+        penguinCarouselContainer.appendChild(
+            createImage(penguinsArray[currentPenguinImage])
+        );
+    }
+
+    const previous = document.createElement('img');
+    const next = document.createElement('img');
+
+    previous.src = previousArrow;
+    previous.id = 'previous';
+
+    next.src = nextArrow;
+    next.id = 'next';
+
+    createAndAppendPenguin();
+
+    penguinCarouselContainer.append(previous, next);
 }
 
 function createDivContainer(divID) {
