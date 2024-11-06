fb803870f9ed7f4f41c607faf59b6cec14b3d3a9
David Singleton <davidjsingleton18@gmail.com>
2024-11-06 10:59:17 -0800

    Add more penguins for the carousel

--- src/index.js
+++ src/index.js
@@ -3,7 +3,16 @@ import './reset.css';
 import appendPenguinCarousel from './components/PenguinCarouselContainer';
 import boltPenguin from './assets/images/displayed-penguins/bolt-penguin.webp';
 import fluffalPenguin from './assets/images/displayed-penguins/fluffal-penguin.webp';
+import flyingPenguin from './assets/images/displayed-penguins/flying-penguin.webp';
+import glacialBeastPolarPenguin from './assets/images/displayed-penguins/glacial-beast-polar-penguin.webp';
+import guardPenguin from './assets/images/displayed-penguins/guard-penguin.webp';
 
-const penguinImages = [boltPenguin, fluffalPenguin];
+const penguinImages = [
+    boltPenguin,
+    fluffalPenguin,
+    flyingPenguin,
+    glacialBeastPolarPenguin,
+    guardPenguin
+];
 
 appendPenguinCarousel(penguinImages);
