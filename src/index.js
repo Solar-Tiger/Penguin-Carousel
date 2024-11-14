50d70a756faca4a49b5dc108ff7ecb6910c9db22
David Singleton <davidjsingleton18@gmail.com>
2024-11-14 10:50:52 -0800

    Update file names to be more carousel universal

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './style.css';
 import './reset.css';
-import appendPenguinCarousel from './components/PenguinCarouselContainer';
+import { createCarousel } from './components/CarouselContainer';
+import { carouselControls as changeDisplayedPenguin } from './modules/carouselControls';
 import boltPenguin from './assets/images/displayed-penguins/bolt-penguin.webp';
 import fluffalPenguin from './assets/images/displayed-penguins/fluffal-penguin.webp';
 import flyingPenguin from './assets/images/displayed-penguins/flying-penguin.webp';
@@ -15,4 +16,5 @@ const penguinImages = [
     guardPenguin
 ];
 
-appendPenguinCarousel(penguinImages);
+createCarousel(penguinImages);
+changeDisplayedPenguin(penguinImages);
