5b840260b88a821c0f38de0f7982773ae1628a60
David Singleton <davidjsingleton18@gmail.com>
2024-12-09 10:55:12 -0800

    Update image position based on size of images for dynamic sizing on any device

--- src/modules/carouselControls.js
+++ src/modules/carouselControls.js
@@ -2,6 +2,7 @@ let currentImageIndex = 0;
 let currentDisplayedImagePosition = 0;
 
 function carouselControls(imageArray, intervalTime = 5000) {
+    const carouselImage = document.querySelector('.carousel-image-frame img');
     const carouselImageFrame = document.querySelector('.carousel-image-frame');
     const nextCarouselBtn = document.querySelector('.next-button');
     const previousCarouselBtn = document.querySelector('.previous-button');
@@ -11,13 +12,13 @@ function carouselControls(imageArray, intervalTime = 5000) {
     const carouselDotsSelected = document.querySelectorAll('.carousel-dot');
 
     nextCarouselBtn.addEventListener('click', () => {
-        currentDisplayedImagePosition += -295;
+        currentDisplayedImagePosition += -carouselImage.clientWidth - 5;
 
         carouselImageFrame.style.transform = `translateX(${currentDisplayedImagePosition}px)`;
     });
 
     previousCarouselBtn.addEventListener('click', () => {
-        currentDisplayedImagePosition += 295;
+        currentDisplayedImagePosition += carouselImage.clientWidth + 5;
 
         carouselImageFrame.style.transform = `translateX(${currentDisplayedImagePosition}px)`;
     });
